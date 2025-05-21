955e42eabbaef98ad4f601c90e4781da23369b63
Trương Nguyễn Hương Giang <143255970+GiangNguyen0208@users.noreply.github.com>
2025-05-21 21:22:22 +0700

    create .env file

--- src/main/java/com/example/Jewelry/JewelryApplication.java
+++ src/main/java/com/example/Jewelry/JewelryApplication.java
@@ -3,6 +3,7 @@ package com.example.Jewelry;
 import com.example.Jewelry.Utility.Constant;
 import com.example.Jewelry.entity.User;
 import com.example.Jewelry.service.UserService;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,13 @@ public class JewelryApplication implements CommandLineRunner {
 	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+		System.setProperty("FACEBOOK_CLIENT_ID", dotenv.get("FACEBOOK_CLIENT_ID"));
+		System.setProperty("FACEBOOK_CLIENT_SECRET", dotenv.get("FACEBOOK_CLIENT_SECRET"));
+
 		SpringApplication.run(JewelryApplication.class, args);
 	}
 
